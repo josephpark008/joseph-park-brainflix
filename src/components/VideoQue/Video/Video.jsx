8e468da8a23502eb955605c8f6df0f9f80f418fc
Joseph Park <josephpark008@gmail.com>
2024-03-10 19:09:51 -0400

    Added functionality to video selection

--- src/components/VideoQue/Video/Video.jsx
+++ src/components/VideoQue/Video/Video.jsx
@@ -1,14 +1,23 @@
 import "./Video.scss";
 export default function Video(props) {
-  const video = props.videoData;
+  const { videoData, displayVideo } = props;
 
   return (
-    <div className="video">
-      <img className="video__image" src={video.image} alt="thumbnail image" />
+    <div
+      className="video"
+      onClick={() => {
+        displayVideo(videoData.id);
+      }}
+    >
+      <img
+        className="video__image"
+        src={videoData.image}
+        alt="thumbnail image"
+      />
 
       <div className="video-text">
-        <h1 className="video-text__title">{video.title}</h1>
-        <p className="video-text__creator">{video.channel}</p>
+        <h1 className="video-text__title">{videoData.title}</h1>
+        <p className="video-text__creator">{videoData.channel}</p>
       </div>
     </div>
   );
